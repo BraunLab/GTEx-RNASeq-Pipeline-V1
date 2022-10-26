8c8886e4dd618c6d1a53eab34a964cb4f788a89a
pdeepakb <poduval.deepak.b@gmail.com>
2022-10-26 14:39:37 -0400

    Commit for gtex_pipeline - all files

---
+++ gtex_rnaseq_pipeline_bam_v1/wf_gtex_rnaseq.v1.sh
@@ -0,0 +1,102 @@
+###########################################################################################################################################
+# Author: Deepak B Poduval																												  #
+# Date: 10/14/22																														  #
+# Location: Braun Lab, Yale University																									  #
+# Filename: wf_gtex_rnaseq.v1.sh														  												  #
+#																																		  #
+# In-house RNA Seq pipeline based on GTEx v10 release. - Version 1									  									  #
+# This workflow is used along with the slurm script that makes joblists - wf_gtex_rnaseq_joblist.v1.slurm		  		 		  #
+# No changes need to be made																											  #
+# Please copy all the files from gtex_rnaseq_pipeline_v1, including references to the folder with bam files, for this script to work. 	  # 
+###########################################################################################################################################
+
+#!/bin/bash -ue
+
+##
+# The Path block to setup path of folders and files - input and outputs
+############################################################################################################################################
+
+##Path of the working folder
+path=`pwd`
+
+##Path and name for the results folder
+out="$path/results"
+
+##Crosscheck whether the folder was created
+if [ ! -d "$out" ]; then
+	mkdir  -p $out
+fi
+
+##Path to folder with all the references
+reference="$path/references"
+
+##Collapsed gene model references
+gtf="$reference/GRCh38.gencode.collapsed.gtf"
+
+##Takes input bam file name as a command line argument
+in_file=$1
+
+##Takes part of the name to name the outputs based on sample names.
+out_name=${in_file%.b*}
+
+#Makes specific output folder with sample name
+mkdir $out/$out_name
+
+##------------------------------------------------------------------------------------------------------------------------------------------
+
+##
+# The Log block to print the versions of the program into a log file
+##
+#############################################################################################################################################
+
+log="$out/$out_name/$out_name"_program_version.log
+
+echo "Versions log" > $log
+
+echo -e "GTEx RNA-Seq Pipeline Version 1\n using GTEx Release V10\n" >>$log
+echo -e "genome-build GRCh38.p10\tgenome-version GRCh38\tgenome-build-accession NCBI:GCA_000001405.25\n" >>$log
+echo -e "Gene Model used is collapsed using custom script from GTEx\n" >>$log
+ 
+echo "SamToFastq " >>$log 
+apptainer exec $path/gtex_rnaseq_v10.sif java -jar -Xmx${SLURM_MEM_PER_NODE} /opt/picard-tools/picard.jar SamToFastq --version True >>$log
+
+echo "STAR Version:" >>$log
+apptainer exec $path/gtex_rnaseq_v10.sif STAR --version >>$log
+
+echo "Samtools Version:" >>$log
+apptainer exec $path/gtex_rnaseq_v10.sif samtools --version >>$log
+
+echo "Mark Duplicates " >>$log
+apptainer exec $path/gtex_rnaseq_v10.sif java -jar -Xmx${SLURM_MEM_PER_NODE} /opt/picard-tools/picard.jar MarkDuplicates --version True >>$log
+
+echo "RNA-SeQC Version:  " >>$log
+apptainer exec $path/gtex_rnaseq_v10.sif rnaseqc --version >>$log
+
+echo "RSEM Version:  " >>$log
+apptainer exec $path/gtex_rnaseq_v10.sif rsem-calculate-expression --version >>$log
+
+##------------------------------------------------------------------------------------------------------------------------------------------
+
+##
+# The Workflow block creates from BAM/FASTQ to RSEM Matrix
+#############################################################################################################################################
+
+# BAM to FASTQ conversion
+time apptainer exec $path/gtex_rnaseq_v10.sif /src/run_SamToFastq.py $path/$in_file -p $out_name -o $out/$out_name
+
+# STAR alignment
+time apptainer exec $path/gtex_rnaseq_v10.sif /src/run_STAR.py $reference/star_index/ $out/$out_name/$out_name"_1.fastq.gz" $out/$out_name/$out_name"_2.fastq.gz" $out_name -t ${SLURM_CPUS_PER_TASK} -o $out/$out_name/star_out
+
+# sync BAMs (copy QC flags and read group IDs)
+time apptainer exec $path/gtex_rnaseq_v10.sif /src/run_bamsync.sh $path/$in_file $out/$out_name/star_out/$out_name".Aligned.sortedByCoord.out.bam" $out/$out_name/star_out/$out_name
+
+# mark duplicates (Picard)
+time apptainer exec $path/gtex_rnaseq_v10.sif /src/run_MarkDuplicates.py $out/$out_name/star_out/$out_name".Aligned.sortedByCoord.out.patched.bam" $out/$out_name/$out_name".Aligned.sortedByCoord.out.patched.md" -o $out/$out_name
+
+# RNA-SeQC
+time apptainer exec $path/gtex_rnaseq_v10.sif /src/run_rnaseqc.py $gtf $out/$out_name/$out_name".Aligned.sortedByCoord.out.patched.md.bam" $out_name -o $out/$out_name
+
+# RSEM transcript quantification
+time apptainer exec $path/gtex_rnaseq_v10.sif /src/run_RSEM.py $reference/rsem_reference $out/$out_name/star_out/$out_name".Aligned.toTranscriptome.out.bam" $out/$out_name/$out_name -t ${SLURM_CPUS_PER_TASK}
+
+##------------------------------------------------------------------------------------------------------------------------------------------
---
+++ gtex_rnaseq_pipeline_fq_v1/README.md
@@ -0,0 +1,74 @@
+# GTEx RNA-Seq Pipeline - Fastq - Version 1 
+
+## README
+
+## TL;DR
+
+This directory a workflow bash script, two slurm scipts. This need to be run inside the directory containing fastq files, references folder and singularity container for the pipeline on farnam HPC cluster at Yale (Not tested).
+
+## Directory
+
+### references
+
+The directory contains genome fasta file, indices and genes gtf file for human version GRCh38.p10 (NCBI:GCA_000001405.25). A gene model for the gtf file made using script from GTEx. Two other folders of reference indices one for STAR, mapping reads and RSEM, quantification. This gives flexibility to change references to a newer version or different version for other purposes.
+
+## Singularity container
+
+### gtex_rnaseq_v10.sif
+
+This is the singularity container that inculdes all the neccessary helper scripts and programs that need to run the RNA-Seq analysis including STAR, picard, RNA-SeQC2 and RSEM. The container also has helper scripts to combine the results form individual runs.
+
+**Note: Copy the references and singularity container from "/SAY/standard/braunlab-CC1413-MEDCCC/BraunLab_pipelines/gtex_rnaseq_pipeline_v1". Also the appropriate folder of scripts are present at the same locations. A copy of the scripts are also available at <https://github.com/BraunLab/gtex_rnaseq_pipeline_v1>.**
+
+## Bash script
+
+### wf_gtex_rnaseq.v1.sh
+
+The bash script contain sequential steps required to do the RNA-Seq analysis from fastq to count matrix files for individual fastq files. This script takes input fastq file and and runs all essential steps including creating an output log file, for genome version and program version that was run.
+
+## Slurm scripts
+
+Slurm is a scheduler used in farnam to schedule jobs.
+
+### wf_gtex_rnaseq_joblist.v1.slurm
+
+Farnam uses dSQ scheduler to serialise jobs. This slurm script will loop through the names of fastq files making a joblist txt file, with eachline containing execution of workflow bash script for each fastq file. It will submit the joblist to dSQ scheduler outputing the submission line to run the whole pipeline in serial.
+
+### wf_gtex_rnaseq_aggregate.v1.slurm
+
+This slurm script is used to make result aggregate from RSEM and RNASeQC2 runs using helper scripts.
+
+## Instructions
+
+Download all the contents of this directory to your destinatation folder with all the fastq files to analyze.
+The first step in the workflow is to run
+
+```bash
+sbatch wf_gtex_rnaseq_joblist.v1.slurm
+```
+
+This step will create a joblist file and submit it to dSQ serial job submission script with a date of run.
+e.g. dsq-joblist-2022-09-14.sh
+It will output a message for exact command to run.
+
+```bash
+sbatch dsq-joblist-2022-09-14.sh
+```
+
+This block of code will run the workflow on all the fastq files with available resources in farnam with a "jobid". To check the progress of the run the code block below. It will give you status of the progress.
+
+```bash
+module load dSQ
+dsqa -j <jobid>
+```
+
+If the run fails due to resource inavailability. If you haven't loaded dSQ module load it again and run the code block below. That should rerun all the jobs that failed.
+Please refer to <https://docs.ycrc.yale.edu/clusters-at-yale/job-scheduling/dsq/> to learn more about dSQ.
+
+```bash
+#module load dSQ
+dsqa -j <jobid> -f jobsfile.txt > re-run_jobs.txt 2> <jobid>_report.txt
+dsq --job-file re-run_jobs.txt --time=2-00:00:00 --cpus-per-task=5 --mem=35G --mail-type=ALL
+```
+
+Please feel free to contact me for any further queries  **deepak(dot)poduval(at)yale(dot)edu**
